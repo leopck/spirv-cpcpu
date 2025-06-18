ca2646e74b114179df9c67d26aec84c5d6d77ee0
William Douglas <william.r.douglas@gmail.com>
2025-06-17 20:53:10 -0700

    Kinda sorta building a graph

--- scheduler.py
+++ scheduler.py
@@ -1,4 +1,5 @@
 import argparse
+import graphlib
 import socket
 
 def parse_args():
@@ -16,6 +17,36 @@ def client_connect(ip, port, infile):
     tcp_socket.close()
 
 
+def process_bin(bin):
+    data = bin.decode('utf-8')
+    try:
+        instructions, text = data.split(':::')
+    except:
+        return
+    funcs = {}
+    flow = graphlib.TopologicalSorter()
+    for instruction in instructions:
+        words = instruction.split()
+        if words[0] == 'FADD' and len(words) == 4:
+            dst = words[1].split('=')[1]
+            src1 = words[2].split('=')[1]
+            src2 = words[3].split('=')[1]
+            flow.add(dst, src1, src2)
+            func = lambda x, y: x + y
+            if dfunc := funcs.get(dst):
+                dfunc.insert(0, func)
+            else:
+                funcs[dst] = [func]
+        else:
+            return
+    flow.prepare()
+    dp = 0
+    while flow.is_active():
+        for node in flow.get_ready():
+            # process
+            pass
+
+
 def server_host(ip, port):
     tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     tcp_socket.bind((ip, port))
@@ -23,15 +54,14 @@ def server_host(ip, port):
 
     while True:
         conn, client = tcp_socket.accept()
-        sndfile = b""
+        bin = b""
         while True:
             data = conn.recv(4096)
             if not data:
                 break
-            sndfile += data
-        if sndfile:
-            # do stuff with the bin
-            pass
+            bin += data
+        if bin:
+            process_bin(bin)
 
 
 def main():
